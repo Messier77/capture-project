f63918463d7da2a184d3723ee3d86bf61c0cffd2
Messier77 <Messier77@users.noreply.github.com>
2022-01-14 20:07:31 +0200

    3. services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 // page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />;
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
