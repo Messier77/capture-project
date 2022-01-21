5551122031ca5f5b2ee6c8f99dc4ee18c8d7db8b
Messier77 <Messier77@users.noreply.github.com>
2022-01-21 21:09:58 +0200

    11. movie details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GlobalStyle from "./components/GlobalStyle";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 // router
 import { Route, Routes } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<AboutUs />} />
         <Route path="/work" exact element={<OurWork />} />
+        <Route path="/work/:id" exact element={<MovieDetail />} />
         <Route path="/contact" exact element={<ContactUs />} />
       </Routes>
     </div>
