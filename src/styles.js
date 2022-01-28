4e6af3f681c87fc2bce8b944ca027bba6de05609
Messier77 <Messier77@users.noreply.github.com>
2022-01-28 15:29:04 +0200

    23. mobile friendly

--- src/styles.js
+++ src/styles.js
@@ -8,6 +8,11 @@ export const About = styled(motion.div)`
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+  @media (max-width: 1300px) {
+    display: block;
+    padding: 0;
+    text-align: center;
+  }
 `;
 
 export const Description = styled.div`
@@ -16,6 +21,12 @@ export const Description = styled.div`
   z-index: 2;
   h2 {
     font-weight: lighter;
+  }
+  @media (max-width: 1300px) {
+    padding: 2rem 2rem;
+    button {
+      margin: 2rem 0rem 5rem 0rem;
+    }
   }
 `;
 
@@ -27,6 +38,10 @@ export const Image = styled.div`
     width: 100%;
     height: 80vh;
     object-fit: cover;
+    @media (max-width: 1300px) {
+      padding: 0;
+      margin: 0;
+    }
   }
 `;
 
